15b17478fa39abbfebee612c378a40681a298a21
realzu <jinhh501@naver.com>
2026-03-16 20:47:30 +0900

    chore: github pages 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,13 @@ import react from '@vitejs/plugin-react-swc';
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    base: '/I_SEOUL_U_3-2/',
   }),
   defineTestConfig({
     test: {
       globals: true,
       environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+      setupFiles: './src/setupTests.ts',
     },
-  })
-)
+  }),
+);
